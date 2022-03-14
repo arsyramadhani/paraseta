e75370c6de341598b44dc6049bd2a05387184a83
Febrian Arsy Ramadhani <64425906+arsyramadhani@users.noreply.github.com>
2022-03-14 23:54:10 +0700

    Update api.js

--- services/api.js
+++ services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'paraseta.vercel.app';
+//axios.defaults.baseURL = 'paraseta.vercel.app';
 
 export async function fetchWorkspace() {
     return await axios.get('/api/workspace').then(res => res.data.workspaces);
